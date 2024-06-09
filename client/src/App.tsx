c03eeecc5dbf707b2f80b9678340812fe9de0270
anuj <anujsbhuyar@gmail.com>
2024-06-09 13:11:50 +0530

    init client side

---
+++ client/src/App.tsx
@@ -0,0 +1,22 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Landing from "./components/Landing";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              <Landing />
+            </div>
+          }
+        />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
