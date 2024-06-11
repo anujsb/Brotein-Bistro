f22473170c59f67944ecd66fcb1848ed9b3285e1
anuj <anujsbhuyar@gmail.com>
2024-06-11 23:47:35 +0530

    init

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Landing from "./components/Landing";
+import Landing from "./components/pages/Landing";
 
 function App() {
   return (
