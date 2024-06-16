611121414857c243b2a770e65cce489b09eea9da
anuj <anujsbhuyar@gmail.com>
2024-06-16 18:10:53 +0530

    hero mobile

--- src/components/Landing/NavBarLanding.tsx
+++ src/components/Landing/NavBarLanding.tsx
@@ -1,32 +1,58 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className="w-screen max-h-20 backdrop-blur-sm shadow-md fixed grid grid-flow-col">
-      {/* <div className="flex flex-row w-full"> */}
-      <div className="flex justify-start items-center">
+      <div className="flex justify-between items-center w-full p-4">
         <img src="logo.png" alt="logo" className="h-14 max-h-16 m-1" />
-      </div>
-      <div className="flex justify-end items-center mx-5 ">
-        <Link
-          className="bg-[#FBE7CB] rounded-full font-semibold py-2 px-4 mr-5 shadow-md"
-          role="button"
-          to="/"
-        >
-          Order Now
-        </Link>
-        <Link
-          className="rounded-full font-semibold  py-2 px-4 mr-5 border-black border shadow-md"
-          role="button"
-          to="/"
-        >
-          See Plans
-        </Link>
-        <div>
-          <img className="w-10 h-10" src="ham.svg" alt="ham" role="button" />
+        <div className="md:hidden">
+          <button
+            onClick={() => setIsOpen(!isOpen)}
+            className="focus:outline-none"
+          >
+            <img className="w-10 h-10" src="ham.svg" alt="ham" />
+          </button>
+        </div>
+        <div className="hidden md:flex justify-end items-center space-x-5 mx-5">
+          <Link
+            className="bg-[#FBE7CB] rounded-full font-semibold py-2 px-4 shadow-md"
+            role="button"
+            to="/"
+          >
+            Order Now
+          </Link>
+          <Link
+            className="rounded-full font-semibold py-2 px-4 border-black border shadow-md"
+            role="button"
+            to="/"
+          >
+            See Plans
+          </Link>
         </div>
       </div>
-      {/* </div> */}
+      {isOpen && (
+        <div className="md:hidden flex flex-col items-center bg-white w-full shadow-md">
+          <Link
+            className="bg-[#FBE7CB] rounded-full font-semibold py-2 px-4 my-2 shadow-md w-11/12 text-center"
+            role="button"
+            to="/"
+            onClick={() => setIsOpen(false)}
+          >
+            Order Now
+          </Link>
+          <Link
+            className="rounded-full font-semibold py-2 px-4 my-2 border-black border shadow-md w-11/12 text-center"
+            role="button"
+            to="/"
+            onClick={() => setIsOpen(false)}
+          >
+            See Plans
+          </Link>
+        </div>
+      )}
     </nav>
   );
 };
