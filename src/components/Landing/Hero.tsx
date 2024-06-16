ca60c69af56c461d1611311bbd749a584267b6ce
anuj <anujsbhuyar@gmail.com>
2024-06-16 19:20:06 +0530

    res5

--- src/components/Landing/Hero.tsx
+++ src/components/Landing/Hero.tsx
@@ -9,10 +9,11 @@ const Hero = () => {
           {/* <img className="h-screen " src="line.png" alt="line" /> */}
           <img className="bg-hero " src="hero6.png" alt="hero-image" />
         </div>
-        <div className=" order-2 md:order-1">
-          <div className="lg:mt-20 md:mt-20 sm:mt-5 flex flex-col items-center">
-            <h1 className="text-5xl font-bold text-center">
-              The <br /> Muscle <br /> Friendly <br /> Bistro
+        <div className=" order-2 md:order-1 flex flex-col justify-center items-center">
+          {/* <div className="lg:mt-20 md:mt-20 sm:mt-0 flex flex-col items-center"> */}
+          {/* <div className=" "> */}
+            <h1 className="text-6xl font-bold ">
+              The Muscle <br /> Friendly Bistro
             </h1>
 
             <div className="mt-10">
@@ -31,7 +32,7 @@ const Hero = () => {
                 See Plans
               </Link>
             </div>
-          </div>
+          {/* </div> */}
         </div>
       </div>
     </section>
