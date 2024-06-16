0cd1da54300715829f997c93f1e84db058f115f8
anuj <anujsbhuyar@gmail.com>
2024-06-16 19:13:05 +0530

    res3

--- src/components/Landing/Hero.tsx
+++ src/components/Landing/Hero.tsx
@@ -5,33 +5,32 @@ const Hero = () => {
     <section className="w-full h-screen  snap-start bg ">
       {/* <div className="grid md:grid-flow-col lg:grid-flow-col sm:grid-flow-row md:grid-cols-2 lg:grid-cols-2 h-screen"> */}
       <div className="grid h-screen sm:grid-flow-row md:grid-flow-col lg:grid-flow-col sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
-        <div className="h-full flex justify-end order-1 md:order-2">
+        <div className="flex justify-end order-1 md:order-2">
           {/* <img className="h-screen " src="line.png" alt="line" /> */}
           <img className="bg-hero " src="hero6.png" alt="hero-image" />
         </div>
-        <div className=" order-2 md:order-1 md:p-40 lg:p-40 sm:p-10">
-          <h1 className="text-6xl font-bold">
-            The <br /> Muscle <br /> Friendly <br /> Bistro
-          </h1>
-          {/* <p className=" ">
-            Personalized diet plans
-            <br /> delivered to your door.
-          </p> */}
-          <div className="mt-10">
-            <Link
-              className="bg-[#FBE7CB] rounded-full font-semibold py-2 px-4 mr-5 shadow-md"
-              role="button"
-              to="/"
-            >
-              Order Now
-            </Link>
-            <Link
-              className="rounded-full font-semibold py-2 px-4 mr-5 border-black border shadow-md"
-              role="button"
-              to="/"
-            >
-              See Plans
-            </Link>
+        <div className=" order-2 md:order-1">
+          <div className="lg:mt-20 md:mt-20 sm:mt-5 flex flex-col items-center">
+            <h1 className="text-5xl font-bold text-center">
+              The <br /> Muscle <br /> Friendly <br /> Bistro
+            </h1>
+
+            <div className="mt-10">
+              <Link
+                className="bg-[#FBE7CB] rounded-full font-semibold py-2 px-4 mr-5 shadow-md"
+                role="button"
+                to="/"
+              >
+                Order Now
+              </Link>
+              <Link
+                className="rounded-full font-semibold py-2 px-4 mr-5 border-black border shadow-md"
+                role="button"
+                to="/"
+              >
+                See Plans
+              </Link>
+            </div>
           </div>
         </div>
       </div>
