d82a9fb49e0cf34b6a3fea23c40c3c3f8c7da86c
anuj <anujsbhuyar@gmail.com>
2024-06-20 14:39:23 +0530

    products added

--- src/components/Landing/Footer.tsx
+++ src/components/Landing/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <div className="snap-end">
       <div className="relative bg-blueGray-200 pt-8 pb-6">
-        <div className="container  px-4">
+        <div className="container  md:px-4">
           <div className="flex flex-wrap text-left lg:text-left">
             <div className="w-full lg:w-6/12 px-4 mb-6 lg:mb-0">
               <h4 className="text-3xl font-semibold text-blueGray-700">
@@ -125,7 +125,7 @@ const Footer = () => {
           <div className="flex flex-wrap items-center md:justify-between justify-center">
             <div className="w-full md:w-4/12 px-4 mx-auto text-center">
               <div className="text-sm text-blueGray-500 font-semibold py-1">
-                Copyright © <span id="get-current-year">2021</span>
+                Copyright © <span id="get-current-year">2024</span>
                 <a
                   href="https://www.creative-tim.com/product/notus-js"
                   className="text-blueGray-500 hover:text-gray-800"
--- src/components/Landing/Products.tsx
+++ src/components/Landing/Products.tsx
@@ -6,7 +6,7 @@ const Products = () => {
       <div className="mb-10">
         <h1 className="text-5xl font-bold text-gray-800">Our Services</h1>
       </div>
-      <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-8 w-full max-w-7xl">
+      <div className="grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 gap-8 w-full max-w-7xl">
         <div className="bg-white p-6 rounded-xl shadow-lg hover:shadow-2xl transition-shadow duration-300">
           <img
             src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
@@ -36,6 +36,21 @@ const Products = () => {
           <Link to="/" className="text-black mt-4 inline-block">
             Learn More
           </Link>
+        </div>
+        <div className="bg-white p-6 rounded-xl shadow-lg hover:shadow-2xl transition-shadow duration-300">
+          <img
+            src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
+            alt="card-image"
+            className="rounded-t-xl w-full h-48 object-cover"
+          />
+          <h2 className="text-2xl font-semibold mt-4 capitalize">
+            Suppliments From leading brand
+          </h2>
+          <p className="text-gray-600 mt-2">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
+            odio.
+          </p>
+          <Link to="/" className="text-black mt-4 inline-block"></Link>
         </div>
         <div className="bg-white p-6 rounded-xl shadow-lg hover:shadow-2xl transition-shadow duration-300">
           <img
