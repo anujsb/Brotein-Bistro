b2f9361b4bdac3a556896539e428cfc93d2a53d2
anuj <anujsbhuyar@gmail.com>
2024-06-30 13:46:47 +0530

    logo

--- src/components/Landing/About.tsx
+++ src/components/Landing/About.tsx
@@ -1,20 +1,18 @@
 const About = () => {
   return (
-    <section className="w-full  snap-start px-10 py-20 ">
-      {/* <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 md:p-20"> */}
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 ">
-        <div>
-          <h1 className="text-slate-500">ABOUT BROTEIN BISTRO</h1>
+    <section className="w-full  snap-start">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div className="lg:flex justify-start hidden md:flex">
+          <img className="h-screen shadow-lg rounded-r-lg " src="/about3.jpg" alt="" />
         </div>
-        <div className="text-4xl md:text-6xl uppercase font-semibold leading-tight col-span-1 md:col-span-2">
-          Empowering your
-          <br /> fitness journey
-        </div>
-
-        <div className="p-4 flex justify-center ">
-          <img className="h-72" src="/about3.jpg" alt="" />
-        </div>
-        <div className="p-4 text-base md:text-">
+        <div className="px-10 py-20">
+          <div>
+            <h1 className="text-slate-500 mb-2">ABOUT BROTEIN BISTRO</h1>
+          </div>
+          <div className="text-4xl md:text-6xl uppercase font-semibold leading-tight col-span-1 md:col-span-2 mb-2">
+            Empowering your
+            <br /> fitness journey
+          </div>
           <p className="m-2">
             At BROTEIN BISTRO, we prioritize health and fitness with a diverse
             menu of nutritious meals and protein-packed smoothies. Enjoy
--- src/components/Landing/NavBarLanding.tsx
+++ src/components/Landing/NavBarLanding.tsx
@@ -5,7 +5,7 @@ const NavBar = () => {
     <nav className="w-screen max-h-20 backdrop-blur-sm shadow-md fixed grid grid-flow-col z-20 ">
       {/* <div className="flex flex-row w-full"> */}
       <div className="flex justify-start items-center">
-        <img src="logo.png" alt="logo" className="h-14 max-h-16 m-1" />
+        <img src="logo.png" alt="logo" className="h-20 max-h-16 m-1" />
       </div>
       <div className="flex justify-end items-center lg:mx-5 md:mx-5 sm:mx-3">
         <Link
